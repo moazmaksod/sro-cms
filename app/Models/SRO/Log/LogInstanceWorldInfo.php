986645f82edcbe82d805c6ea27a4b80fc231d07d
m1xawy <dev.mixawy@gmail.com>
2025-05-02 02:47:12 +0300

    fix: adv unique rank results

--- app/Models/SRO/Log/LogInstanceWorldInfo.php
+++ app/Models/SRO/Log/LogInstanceWorldInfo.php
@@ -45,7 +45,7 @@ class LogInstanceWorldInfo extends Model
         $caseExpression .= 'ELSE 0 END) AS Points';
         $startOfMonth = Carbon::now()->startOfMonth();
 
-        return Cache::remember('ranking_unique_'.$limit.'_'.$month, now()->addMinutes(config('global.general.cache.data.ranking_unique')), function () use ($month, $startOfMonth, $uniquePoints, $caseExpression, $limit) {
+        return Cache::remember("ranking_unique_{$limit}_{$month}", now()->addMinutes(config('global.general.cache.data.ranking_unique')), function () use ($month, $startOfMonth, $uniquePoints, $caseExpression, $limit) {
             return self::join('SILKROAD_R_SHARD.dbo._Char', '_Char.CharID', '=', '_LogInstanceWorldInfo.CharID')
                 ->join('SILKROAD_R_SHARD.dbo._Guild', '_Char.GuildID', '=', '_Guild.ID')
                 ->select(
@@ -69,7 +69,7 @@ class LogInstanceWorldInfo extends Model
                     '_Guild.Name'
                 )
                 ->orderByDesc('Points')
-                ->limit(25)
+                ->limit($limit)
                 ->get();
         });
     }
@@ -77,7 +77,7 @@ class LogInstanceWorldInfo extends Model
     public static function getUniques($limit = 25, $CharID = 0)
     {
         $unique_points = array_keys(config('global.ranking.unique_points'));
-        return Cache::remember('unique_history_'.$limit.'_'.$CharID, now()->addMinutes(config('global.general.cache.data.unique_history')), function () use ($CharID, $limit, $unique_points) {
+        return Cache::remember("unique_history_{$limit}_{$CharID}", now()->addMinutes(config('global.general.cache.data.unique_history')), function () use ($CharID, $limit, $unique_points) {
             return self::select(['_LogInstanceWorldInfo.CharID', '_Char.CharName16', '_Char.RefObjID', '_Char.CurLevel', '_LogInstanceWorldInfo.ValueCodeName128', '_LogInstanceWorldInfo.Value', '_LogInstanceWorldInfo.WorldID', '_RefRegion.wRegionID', '_RefRegion.AreaName', '_LogInstanceWorldInfo.EventTime',])
                 ->leftJoin('SILKROAD_R_SHARD.dbo._Char', '_Char.CharID', '=', '_LogInstanceWorldInfo.CharID')
                 ->leftJoin('SILKROAD_R_SHARD.dbo._RefRegion', '_RefRegion.wRegionID', '=', '_LogInstanceWorldInfo.WorldID')
