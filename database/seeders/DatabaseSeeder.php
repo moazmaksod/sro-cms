0dcc5a35e15f59df9effb3f6bb8f80cd9d408f8d
m1xawy <dev.mixawy@gmail.com>
2025-05-03 16:49:47 +0300

    fix: run seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -22,8 +22,11 @@ class DatabaseSeeder extends Seeder
         //     'email' => 'test@example.com',
         // ]);
 
-        //$this->call(MagOptSeeder::class);
-        //$this->call(ItemNameSeeder::class);
+        $this->call(MagOptSeeder::class);
+        $this->call(ItemNameSeeder::class);
+        $this->call(LogInstanceWorldInfo::class);
+        $this->call(LogChatMessage::class);
+
         $this->call(SettingSeeder::class);
         $this->call(DonationMethodsSeeder::class);
         $this->call(MaxiCardSeeder::class);
