d1a72374cd269a8c6816de901a977ca18be3e5ba
m1xawy <dev.mixawy@gmail.com>
2025-05-26 09:36:54 +0300

    apt: change item details message

--- resources/views/ranking/character/partials/inventory/item-details.blade.php
+++ resources/views/ranking/character/partials/inventory/item-details.blade.php
@@ -22,7 +22,7 @@
         @if(!config("settings.item_stats_jid_{$userJID}"))
             @include('ranking.character.partials.inventory.item-blues-whites', ['item' => $item['ItemInfo']])
         @else
-            <span style="color:#ff2f51">{{ __('The Information Hidden by User') }}</span><br />
+            <span style="color:#ff2f51">{{ __('Item details are restricted by User') }}</span><br />
         @endif
     </div>
     <?php endif; ?>
