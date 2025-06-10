dc219923b969c268ef9007a6f34f2c4db77c841c
m1xawy <dev.mixawy@gmail.com>
2025-06-10 12:21:38 +0300

    fix: admin panel icons & add button to back home

--- resources/views/admin/layouts/sidebar.blade.php
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -12,6 +12,12 @@
                 </a>
             </li>
             <li class="nav-item">
+                <a class="nav-link d-flex align-items-center gap-2 {{ request()->routeIs('admin.users.index') ? 'active' : '' }}" href="{{ route('admin.users.index') }}">
+                    <svg class="bi" aria-hidden="true"><use xlink:href="#people"/></svg>
+                    {{ __('Users') }}
+                </a>
+            </li>
+            <li class="nav-item">
                 <a class="nav-link d-flex align-items-center gap-2 {{ request()->routeIs('admin.news.index') ? 'active' : '' }}" href="{{ route('admin.news.index') }}">
                     <svg class="bi" aria-hidden="true"><use xlink:href="#file-earmark"/></svg>
                     {{ __('News') }}
@@ -19,32 +25,37 @@
             </li>
             <li class="nav-item">
                 <a class="nav-link d-flex align-items-center gap-2 {{ request()->routeIs('admin.download.index') ? 'active' : '' }}" href="{{ route('admin.download.index') }}">
-                    <svg class="bi" aria-hidden="true"><use xlink:href="#cart"/></svg>
+                    <svg class="bi" aria-hidden="true"><use xlink:href="#file-earmark"/></svg>
                     {{ __('Download') }}
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link d-flex align-items-center gap-2 {{ request()->routeIs('admin.pages.index') ? 'active' : '' }}" href="{{ route('admin.pages.index') }}">
-                    <svg class="bi" aria-hidden="true"><use xlink:href="#graph-up"/></svg>
+                    <svg class="bi" aria-hidden="true"><use xlink:href="#file-earmark"/></svg>
                     {{ __('Pages') }}
                 </a>
             </li>
+        </ul>
+
+        <h6 class="sidebar-heading d-flex justify-content-between align-items-center px-3 mt-4 mb-1 text-body-secondary text-uppercase">
+            <span>Logs</span>
+            <!--
+            <a class="link-secondary" href="#" aria-label="Add a new report">
+                <svg class="bi" aria-hidden="true"><use xlink:href="#plus-circle"></use></svg>
+            </a>
+            -->
+        </h6>
+        <ul class="nav flex-column mb-auto">
             <li class="nav-item">
                 <a class="nav-link d-flex align-items-center gap-2 {{ request()->routeIs('admin.vouchers.index') ? 'active' : '' }}" href="{{ route('admin.vouchers.index') }}">
-                    <svg class="bi" aria-hidden="true"><use xlink:href="#people"/></svg>
+                    <svg class="bi" aria-hidden="true"><use xlink:href="#file-earmark-text"></use></svg>
                     {{ __('Vouchers') }}
                 </a>
             </li>
             <li class="nav-item">
                 <a class="nav-link d-flex align-items-center gap-2 {{ request()->routeIs('admin.donate.logs') ? 'active' : '' }}" href="{{ route('admin.donate.logs') }}">
-                    <svg class="bi" aria-hidden="true"><use xlink:href="#people"/></svg>
+                    <svg class="bi" aria-hidden="true"><use xlink:href="#file-earmark-text"></use></svg>
                     {{ __('Donate Logs') }}
-                </a>
-            </li>
-            <li class="nav-item">
-                <a class="nav-link d-flex align-items-center gap-2 {{ request()->routeIs('admin.users.index') ? 'active' : '' }}" href="{{ route('admin.users.index') }}">
-                    <svg class="bi" aria-hidden="true"><use xlink:href="#people"/></svg>
-                    {{ __('Users') }}
                 </a>
             </li>
         </ul>
@@ -59,9 +70,9 @@
                 </a>
             </li>
             <li class="nav-item">
-                <form method="POST" action="{{ route('logout') }}">
+                <form method="POST" action="{{ route('logout') }}" onsubmit="return confirm('Are you sure you want to Sign out?')">
                     @csrf
-                    <a class="nav-link d-flex align-items-center gap-2" href="{{ route('logout') }}" onsubmit="return confirm('Are you sure you want to Sign out?')" onclick="event.preventDefault();this.closest('form').submit();">
+                    <a class="nav-link text-danger d-flex align-items-center gap-2" href="{{ route('logout') }}" onclick="event.preventDefault();this.closest('form').submit();">
                         <svg class="bi" aria-hidden="true"><use xlink:href="#door-closed"/></svg>
                         {{ __('Log Out') }}
                     </a>
